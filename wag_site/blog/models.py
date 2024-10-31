270e92ccddd789c7263af09e324ea3fde9782435
alaa samy <jopa.samy2020@gmail.com>
2024-10-31 18:33:16 +0300

    add date in all blog section and link with catagory

--- wag_site/blog/models.py
+++ wag_site/blog/models.py
@@ -69,13 +69,13 @@ class BlogDetailPage(Page):
     # blog_start_date = models.DateTimeField()  # تأكد من أن الحقل موجود في النموذج
     blog_type = models.CharField(max_length=255)
     blog_date = models.DateTimeField("Publication Date", blank=True, null=True)  # أضف حقل التاريخ هنا
-    # category = models.ForeignKey(  # تغيير الاسم إلى 'category'
-    #     'idec.CategoryPage', 
-    #     null=True,  # السماح بقيمة null
-    #     blank=True,  # السماح بترك الحقل فارغًا
-    #     on_delete=models.SET_NULL,  # تعيينه إلى null إذا تم حذف الصفحة المرتبطة
-    #     related_name='+'
-    # )
+    category = models.ForeignKey(  # تغيير الاسم إلى 'category'
+        'idec.CategoryPage', 
+        null=True,  # السماح بقيمة null
+        blank=True,  # السماح بترك الحقل فارغًا
+        on_delete=models.SET_NULL,  # تعيينه إلى null إذا تم حذف الصفحة المرتبطة
+        related_name='+'
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel('blog_title'),
@@ -85,7 +85,7 @@ class BlogDetailPage(Page):
         FieldPanel('blog_type'),
         InlinePanel('gallery_images_blog', label="Gallery images"),
         FieldPanel('blog_date'),  # إضافة حقل التاريخ إلى اللوحة
-        # PageChooserPanel('category', 'idec.CategoryPage'),  # تغيير هنا إلى 'category'
+        PageChooserPanel('category', 'idec.CategoryPage'),  # تغيير هنا إلى 'category'
 
     ]
 
