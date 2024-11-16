39c3b020d4b886c8562ff52efa4cd489e225d3e8
sayedazp <sayedazp@gmail.com>
2024-11-16 23:28:38 +0200

    initing product model

--- wag_site/product/models/models.py
+++ wag_site/product/models/models.py
@@ -1,3 +1,29 @@
+from django.db import models
 
+# Create your models here.
 
-# class Product(models.mode)
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from wagtail.snippets.models import register_snippet
+from wagtail.models import AbstractPage, ClusterableModel
+# from wagtail.models import PageBase
+from wagtail.admin.panels import FieldPanel
+from wagtailmetadata.models import MetadataPageMixin
+# from .pages import Category_index_page
+# from wagtail.models import Page
+# class Categoryy_index_page(Page):
+#     pass
+from categorization.models.models import CategoryMp
+
+@register_snippet
+class Product(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    category = models.OneToOneField(CategoryMp, on_delete=models.DO_NOTHING, related_name="prod_page")
+    
+    panels = [
+        FieldPanel("name"),
+    ]
+
+    # def __str__(self):
+    #     # print()
+    #     return " >  ".join([a.name for a in self.get_ancestors(include_self=True)])
