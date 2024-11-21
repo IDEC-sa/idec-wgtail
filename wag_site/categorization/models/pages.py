8a1d1df3289b1ab37eeb656a7ac9b6799ea3cf12
Sayed <sayed@sayed.com>
2024-11-21 15:19:31 +0200

    sub-categor-page no prod pagination

--- wag_site/categorization/models/pages.py
+++ wag_site/categorization/models/pages.py
@@ -1,6 +1,7 @@
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 from django.db import models
+from django.db.models import Q
 
 from categorization.models.blocks import Bodycat
 from .models import CategoryMp
@@ -8,7 +9,7 @@ from wagtail.fields import StreamField, RichTextField
 from wagtail_localize.fields import SynchronizedField, TranslatableField
 from product import models as prod_models
 # from product.models.pages import productDetailPage
-from brands.models.pages import BrandsDetailPage
+from brands import models as brand_models
 
 class Category_index_page(Page):
     # unique_id = models.BigAutoField( )
@@ -29,7 +30,7 @@ class Category_index_page(Page):
         products = prod_models.models.Product.objects.filter(category__in=sub_cats).values("id").distinct()
         brands = []
         brands = prod_models.models.Product.objects.filter(category__in=sub_cats).values("brand").distinct()
-        brand_pages =  BrandsDetailPage.objects.filter(brand__in = brands)
+        brand_pages =  brand_models.pages.BrandsDetailPage.objects.filter(brand__in = brands)
         sub_cat_pages = SubPage.objects.filter(cat__in=sub_cats)
         product_pages = prod_models.pages.productDetailPage.objects.filter(product__in=products)[:10]
         print("products")
@@ -41,7 +42,6 @@ class Category_index_page(Page):
         if product_pages:
             ctx["product_pages"] = product_pages
         return ctx
-
 
     override_translatable_fields = [
         TranslatableField("title"),
@@ -58,6 +58,19 @@ class SubPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('cat'),
         FieldPanel('image_logo')
-        # FieldPanel('body'),
-        # FieldPanel('bodycat')
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        ctx =  super().get_context(request, *args, **kwargs)
+        parent = self.cat.parent
+        siblings = parent.get_descendants(include_self=True)
+        products = prod_models.models.Product.objects.filter(Q(category=self.cat)).values("id").distinct()
+        brands = prod_models.models.Product.objects.filter(Q(category=self.cat)).values("brand").distinct()
+        brand_pages = brand_models.pages.BrandsDetailPage.objects.filter(Q(brand__in=brands))
+        product_pages = prod_models.pages.productDetailPage.objects.filter(Q(product__in=products))
+        ctx["cats"] = siblings
+        if brand_pages:
+            ctx['brand_pages'] = brand_pages
+        if product_pages:
+            ctx["product_pages"] = product_pages
+        return ctx
