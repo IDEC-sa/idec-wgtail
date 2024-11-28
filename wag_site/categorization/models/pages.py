33c84d1f291bf654ea321d8bf55bff8a1ff5cdb6
Sayed <sayed@sayed.com>
2024-11-28 09:37:38 +0200

    first edition

--- wag_site/categorization/models/pages.py
+++ wag_site/categorization/models/pages.py
@@ -3,7 +3,7 @@ from wagtail.admin.panels import FieldPanel
 from django.db import models
 from django.db.models import Q
 
-from categorization.models.blocks import Bodycat
+from .blocks import Bodycat, intro_with_background
 from .models import CategoryMp
 from wagtail.fields import StreamField, RichTextField
 from wagtail_localize.fields import SynchronizedField, TranslatableField
@@ -87,6 +87,94 @@ class SubPage(RoutablePageMixin, Page):
             print(additional_filters['brand'])
             print("lol")
             products = products.filter(Q(brand__name=additional_filters['brand']))
+        products = products.values("id").distinct()
+        print(products)
+        return prod_models.pages.productDetailPage.objects.filter(product__in=products)
+
+    def get_paginated_prods(self, qs, page=None):
+        paginator = Paginator(qs, 3)
+        # page = 1
+
+        try:
+            posts = paginator.page(page)
+            print(posts.number)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.object_list.none()
+        return posts
+    
+
+    @re_path(r'^products/$', name="products")
+    def next_page(self, request,):
+        # brand = request.Grequest.GET.get('brand', None)
+        # print(brand)
+        additional_filters = {}
+        brand = request.GET.get('brand', None)
+        print(f"brand{brand}")
+        if brand:
+            print(additional_filters)
+            additional_filters['brand'] = brand
+            
+        return self.render(
+            request,
+            context_overrides={"query":{"brand":brand if brand else None},
+                'product_pages': self.get_paginated_prods(self.get_prods(additional_filters=additional_filters), request.GET.get('pages', 1)
+) ,
+            },
+            template="categorization/paginated_cat_prods.html",
+        )
+
+class brand_landing(RoutablePageMixin, Page):
+    
+    template = 'categorization/sub_page.html'
+    # background_text = models.CharField(max_length=255, blank=True, null=True)
+    # main_text = models.CharField(max_length=255)
+    # brief = models.CharField(max_length=20000, blank=True, null=True)
+    # # brief_part2 = TextBlock(required=False)
+    # image = models.ForeignKey(
+    #     'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    # )
+    brand = models.ForeignKey(to=brand_models.models.Brand, on_delete=models.CASCADE, related_name="brand_landing_page")
+    header_sec = StreamField([("intro_with_background", intro_with_background())], max_num=1, min_num=1)
+
+    image_logo = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel('brand'),
+        FieldPanel('header_sec'),
+        FieldPanel('image_logo')
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        ctx =  super().get_context(request, *args, **kwargs)
+        # parent = self.cat.parent
+        # siblings = parent.get_descendants(include_self=True)
+        cats_ids = prod_models.models.Product.objects.filter(Q(brand=self.brand)).values("category").distinct()
+        cats_children = CategoryMp.objects.filter(id__in=cats_ids)
+        cats = CategoryMp.objects.filter(Q(id__in=cats_ids) | Q(id__in=cats_children.values('parent_id')))
+        # brands = prod_models.models.Product.objects.filter(Q(category=self.cat)).values("brand").distinct()
+        # brand_pages = brand_models.pages.BrandsDetailPage.objects.filter(Q(brand__in=brands))
+        product_pages = self.get_paginated_prods( self.get_prods())
+        ctx["cats"] = cats
+        print(cats)
+        print('categories')
+        # if brand_pages:
+        #     ctx['brand_pages'] = brand_pages
+        if product_pages:
+            ctx["product_pages"] = product_pages
+        print(ctx)
+        return ctx
+
+    def get_prods(self, additional_filters={}):
+        
+        products = prod_models.models.Product.objects.filter(Q(brand=self.brand))
+        print(additional_filters)
+        # if additional_filters.get('brand', None):
+        #     print(additional_filters['brand'])
+        #     print("lol")
+        #     products = products.filter(Q(brand__name=additional_filters['brand']))
         products = products.values("id").distinct()
         print(products)
         return prod_models.pages.productDetailPage.objects.filter(product__in=products)
